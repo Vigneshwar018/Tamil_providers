914ec0778dc12276069338041d1034c22aaec092
Ranjith1310 <Ranjithselvam1396@gmail.com>
2024-11-06 11:14:06 +0530

    fixed TY

--- TamilYogiNewProvider/src/main/kotlin/com/likdev256/TamilYogiNewProvider.kt
+++ TamilYogiNewProvider/src/main/kotlin/com/likdev256/TamilYogiNewProvider.kt
@@ -1,5 +1,5 @@
 package com.likdev256
-
+import android.util.Log
 //import android.util.Log
 import com.lagradost.cloudstream3.*
 //import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
@@ -9,9 +9,9 @@ import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.*
 import org.jsoup.nodes.Element
 
-class TamilYogiProvider : MainAPI() { // all providers must be an instance of MainAPI
+class TamilYogiNewProvider : MainAPI() { // all providers must be an instance of MainAPI
     override var mainUrl = "https://1tamilyogi.vip"
-    override var name = "TamilYogi"
+    override var name = "TamilYogiNew"
     override val hasMainPage = true
     override var lang = "ta"
     override val hasDownloadSupport = true
@@ -23,10 +23,10 @@ class TamilYogiProvider : MainAPI() { // all providers must be an instance of Ma
     //private const val TAG = "TamilYogi"
 
     override val mainPage = mainPageOf(
-        "$mainUrl/category/tamilyogi-full-movie-online/" to "New Movies",
-        "$mainUrl/category/tamil-hd-movies/" to "HD Movies",
-        "$mainUrl/category/tamilyogi-dubbed-movies-online/" to "Dubbed Movies",
-        "$mainUrl/category/tamil-web-series/" to "TV Series"
+        "$mainUrl/tamil-movies-online-tamilvip/" to "New Movies",
+        "$mainUrl/tamil-hd-movies-tamilmv-tamilvip/" to "HD Movies",
+        "$mainUrl/tamil-dubbed-movies-online-one-vip/" to "Dubbed Movies",
+        "$mainUrl/tamilyogi-tamil-web-series-tamilgun-new/" to "TV Series"
     )
 
     override suspend fun getMainPage(
@@ -43,7 +43,7 @@ class TamilYogiProvider : MainAPI() { // all providers must be an instance of Ma
             app.get(request.data + "page/" + page).document
         }
 //        Log.d("CSS element", document.select("ul li").toString())
-        val home = document.select("div.col-lg-4").mapNotNull {
+        val home = document.select("div.grid-items .item").mapNotNull {
             it.toSearchResult()
         }
 
@@ -53,14 +53,14 @@ class TamilYogiProvider : MainAPI() { // all providers must be an instance of Ma
     }
 
     private fun Element.toSearchResult(): SearchResponse? {
-        val titleS = this.selectFirst("div.ghead-dinner > h2.entry-title.grid-title >a")?.text()?.toString()?.trim() ?: return null
+        val titleS = this.selectFirst(".title a")?.text()?.toString()?.trim() ?: return null
         val titleRegex = Regex("(^.*\\)\\d*)")
         val title = titleRegex.find(titleS)?.groups?.get(1)?.value.toString()
-        //Log.d("title", titleS)
+//        Log.d("title", titleS)
         val href = fixUrl(this.selectFirst("a")?.attr("href").toString())
         //Log.d("href", href)
         val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("src"))
-        //Log.d("posterUrl", posterUrl.toString())
+        Log.d("posterUrl", posterUrl.toString())
         val qualityRegex = Regex("(?i)((DVDRip)|(HD)|(HQ)|(HDRip))")
         val qualityN = qualityRegex.find(titleS)?.value.toString()
         //Log.d("QualityN", qualityN)
@@ -87,13 +87,47 @@ class TamilYogiProvider : MainAPI() { // all providers must be an instance of Ma
             this.quality = quality
         } */
     }
+    private fun Element.toSearchResultOnly(): SearchResponse? {
+        val titleS = this.selectFirst("h2 a")?.text()?.toString()?.trim() ?: return null
+        val titleRegex = Regex("(^.*\\)\\d*)")
+        val title = titleRegex.find(titleS)?.groups?.get(1)?.value.toString()
+//        Log.d("title", titleS)
+        val href = fixUrl(this.selectFirst("a")?.attr("href").toString())
+        //Log.d("href", href)
+        val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("src"))
+        Log.d("posterUrl", posterUrl.toString())
+        val qualityRegex = Regex("(?i)((DVDRip)|(HD)|(HQ)|(HDRip))")
+        val qualityN = qualityRegex.find(titleS)?.value.toString()
+        //Log.d("QualityN", qualityN)
+        val quality = getQualityFromString(qualityN)
+        //Log.d("Quality", quality.toString())
+        val checkTvSeriesRegex = Regex("(?i)(E\\s?[0-9]+)|([-]/?[0-9]+)")
+        val isTV = title.contains(checkTvSeriesRegex)
 
+        return if (isTV) {
+            newTvSeriesSearchResponse(title, href, TvType.TvSeries) {
+                this.posterUrl = posterUrl
+                this.quality = quality
+            }
+        } else {
+            newMovieSearchResponse(title, href, TvType.Movie) {
+                this.posterUrl = posterUrl
+                this.quality = quality
+            }
+        }
+
+        /*  return if (isTV == true) {
+          newMovieSearchResponse(title, href, TvType.Movie) {
+              this.posterUrl = posterUrl
+              this.quality = quality
+          } */
+    }
     override suspend fun search(query: String): List<SearchResponse> {
         val document = app.get("$mainUrl/?s=$query").document
-        //Log.d("document", document.toString())
+        Log.d("document", document.toString())
 
-        return document.select("ul li").mapNotNull {
-            it.toSearchResult()
+        return document.select(".recent-posts li").mapNotNull {
+            it.toSearchResultOnly()
         }
     }
 
@@ -104,10 +138,10 @@ class TamilYogiProvider : MainAPI() { // all providers must be an instance of Ma
         val titleRegex = Regex("(^.*\\)\\d*)")
         val rmGibRegex = Regex("(Permanent Link to )")
         val title = rmGibRegex.replace(titleRegex.find(titleL)?.groups?.get(1)?.value.toString(), "")
-        //Log.d("title", title)
+        Log.d("title", title)
         //val titleRegex = Regex()
         //val title =
-        val poster = fixUrlNull(doc.selectFirst("meta[content\$=\".jpg\"]")?.attr("content"))
+        val poster = fixUrlNull(doc.selectFirst("img.size-medium")?.attr("src"))
         //Log.d("poster", poster.toString())
         //val tags = document.select("div.mvici-left p:nth-child(1) a").map { it.text() }
         val yearRegex = Regex("(?<=\\()[\\d(\\]]+(?!=\\))")
@@ -175,12 +209,13 @@ class TamilYogiProvider : MainAPI() { // all providers must be an instance of Ma
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ): Boolean {
-        val linkRegex = Regex("(https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&\\/\\/=]*mp4))")
-        val source = app.get(data).document.select("div.entry iframe").attr("src")
-        val script = app.get(source, referer = "$mainUrl/").document.select("body > script").toString()
+        val linkRegex = Regex("(https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&=,\\/\\/=]*m3u8))")
+        val source = app.get(data).document.select("iframe").attr("src")
+        val script = app.get(source, referer = "$mainUrl/").document.selectFirst("body > script").toString()
         //val links = linkRegex.find(script)?.groups?.get(1)?.value.toString()
         val links = linkRegex.findAll(script).map{it.value.trim()}.toList()
-        //Log.d("links", links.toString())
+Log.d("Iframe", links.toString())
+//        Log.d("linksNew", links.toString())
             safeApiCall {
                 callback.invoke(
                     ExtractorLink(
@@ -188,8 +223,8 @@ class TamilYogiProvider : MainAPI() { // all providers must be an instance of Ma
                         "HD",
                         links[0],
                         "$mainUrl/",
-                        Qualities.P720.value,
-                        false
+                        Qualities.Unknown.value,
+                        true,
                     )
                 )
                 callback.invoke(
